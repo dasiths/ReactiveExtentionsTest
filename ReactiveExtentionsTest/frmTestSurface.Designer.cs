163062319b09a3f301d24b8b08db8e19e99f7d0b
Dasith Wijes <dasith.wijes@readify.net>
2017-03-28 11:18:38 +1100

    Addedpicture box and observed mousemove

--- ReactiveExtentionsTest/frmTestSurface.Designer.cs
+++ ReactiveExtentionsTest/frmTestSurface.Designer.cs
@@ -28,20 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.picInput = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstPoints = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picInput)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picInput
+            // 
+            this.picInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picInput.Location = new System.Drawing.Point(32, 47);
+            this.picInput.Name = "picInput";
+            this.picInput.Size = new System.Drawing.Size(481, 384);
+            this.picInput.TabIndex = 0;
+            this.picInput.TabStop = false;
+            this.picInput.Paint += new System.Windows.Forms.PaintEventHandler(this.picInput_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(526, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Draw Here: Press left button and move mouse inside and outside red box.";
+            // 
+            // lstPoints
+            // 
+            this.lstPoints.FormattingEnabled = true;
+            this.lstPoints.ItemHeight = 20;
+            this.lstPoints.Location = new System.Drawing.Point(541, 47);
+            this.lstPoints.Name = "lstPoints";
+            this.lstPoints.Size = new System.Drawing.Size(258, 384);
+            this.lstPoints.TabIndex = 4;
             // 
             // frmTestSurface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(494, 318);
+            this.ClientSize = new System.Drawing.Size(829, 466);
+            this.Controls.Add(this.lstPoints);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "frmTestSurface";
-            this.Text = "Form1";
+            this.Text = "Test Surface";
+            ((System.ComponentModel.ISupportInitialize)(this.picInput)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstPoints;
     }
 }
 
--- ReactiveExtentionsTest/frmTestSurface.cs
+++ ReactiveExtentionsTest/frmTestSurface.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive;
 
 namespace ReactiveExtentionsTest
 {
@@ -15,6 +18,55 @@ namespace ReactiveExtentionsTest
         public frmTestSurface()
         {
             InitializeComponent();
+
+            //wire up the events as an obeservable stream
+            IObservable<EventPattern<MouseEventArgs>> move =
+                Observable.FromEventPattern<MouseEventArgs>(picInput, "MouseMove");
+
+            //filter only the points inside our rectangle and when mouse is pressed
+            IObservable<System.Drawing.Point> points = from evt in move
+                                                       where ((evt.EventArgs.Location.X >= picInput.Width * 0.1 &&
+                                                       evt.EventArgs.Location.X <= picInput.Width * 0.9) &&
+                                                            (evt.EventArgs.Location.Y >= picInput.Height * 0.1 &&
+                                                       evt.EventArgs.Location.Y <= picInput.Height * 0.9)) &&
+                                                       evt.EventArgs.Button == MouseButtons.Left
+                                                       select evt.EventArgs.Location;
+            points.Subscribe(o => DrawOutput(o));
+        }
+
+        private void picInput_Paint(object sender, PaintEventArgs e)
+        {
+
+            //draw a rectangle 80% of the size of the picture box asour input area
+            e.Graphics.DrawRectangle(Pens.Red, new Rectangle(
+                (int)(picInput.Width * 0.1), (int)(picInput.Height * 0.1),
+                            (int)(picInput.Width * 0.8), (int)(picInput.Height * 0.8)));
+        }
+
+        private void DrawOutput(Point pos)
+        {
+            lstPoints.Items.Add($"Point X({pos.X}) Y({pos.Y})");
+            lstPoints.SelectedIndex = lstPoints.Items.Count - 1;
+
+            if (picInput.Image == null)//if no available bitmap exists on the picturebox to draw on
+            {
+                //create a new bitmap
+                Bitmap bmp = new Bitmap(picInput.Width, picInput.Height);
+
+                picInput.Image = bmp; //assign the picturebox.Image property to the bitmap created
+            }
+
+            using (Graphics g = Graphics.FromImage(picInput.Image))
+            {//we need to create a Graphics object to draw on the picture box, its our main tool
+
+                //when making a Pen object, you can just give it color only or give it color and pen size
+
+                g.DrawEllipse(new Pen(Color.Black, 2), pos.X, pos.Y, 1, 1);
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                //this is to give the drawing a more smoother, less sharper look
+
+                picInput.Invalidate();//refreshes the picturebox
+            }
         }
     }
 }
